acb654cc59e6a9171352495e7c661aab48eec7b8
Anshul Jain <anshul.jain@colorado.edu>
2025-09-07 11:57:27 -0600

    implement get_column function

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,16 @@
+import csv
+
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    results = []
+
+    f = open(file_name, 'r')
+    first_line = True
+    for l in f:
+        if first_line:
+            first_line = False
+            continue
+        A = l.rstrip().split(',')
+        if A[query_column] == query_value:
+            results.append(A[result_column])
+    f.close()
+    return results

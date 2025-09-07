bcfb8559996221ab9a7af466c6eeb38303214c96
Anshul Jain <anshul.jain@colorado.edu>
2025-09-07 12:12:06 -0600

    include comments in my_utils.py, update print_fires.py

--- my_utils.py
+++ my_utils.py
@@ -1,16 +1,17 @@
 import csv
 
+# Implement the get_column function
 def get_column(file_name, query_column, query_value, result_column):
-    results = []
+    results = []                                # array to store results
 
     f = open(file_name, 'r')
     first_line = True
     for l in f:
         if first_line:
-            first_line = False
+            first_line = False                  # skipping the header
             continue
-        A = l.rstrip().split(',')
-        if A[query_column] == query_value:
-            results.append(A[result_column])
+        A = l.rstrip().split(',')               # splitting each row into an array
+        if A[query_column] == query_value:      # comparing query_column with query_value
+            results.append(A[result_column])    # forming the results array
     f.close()
     return results

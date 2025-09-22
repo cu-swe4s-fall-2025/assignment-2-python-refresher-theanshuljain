f5d47aa1876865548f838f62543c6f38090d7d00
Anshul Jain <anshul.jain@colorado.edu>
2025-09-22 00:04:08 -0600

    update run.sh with appropriate examples

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "Working exmaple: "
-python3 main.py --file Agrofood_co2_emission.csv --country "United States of America" --country_column 0 --fires_column 3
+echo "Working example: "
+python3 print_fires.py --file_name Agrofood_co2_emission.csv --country "United States of America" --country_column 0 --fires_column 3
 
 echo "First example with error: "
 # Incorrect file name throws error
-python3 main.py --file Agrofood_emission.csv --country "United States of America" --country_column 0 --fires_column 3
+python3 print_fires.py --file_name Agrofood_emission.csv --country "United States of America" --country_column 0 --fires_column 3
 
 echo "Second example with error: "
 # Incorrect column index throws error
-python3 main.py --file Agrofood_co2_emission.csv --country "United States of America" --country_column 0 --fires_column 33
+python3 print_fires.py --file_name Agrofood_co2_emission.csv --country "United States America" --country_column 0 --fires_column 3
